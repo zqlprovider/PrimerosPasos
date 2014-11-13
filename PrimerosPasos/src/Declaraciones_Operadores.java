8ec9e6045e7c4f3bb0c2f571a734585b77a7fda2
Ezequiel <Ezequiel@Ezequiel-PC>
2014-11-13 19:29:59 -0300

    Original

---
+++ PrimerosPasos/src/Declaraciones_Operadores.java
@@ -0,0 +1,20 @@
+
+public class Declaraciones_Operadores {
+
+	public static void main(String[] args) {
+		
+		double a=5;
+		
+		double b;
+		
+		b=10;
+		
+		double c=b/a;
+		
+		//c-=6;
+		
+		System.out.println(c);
+		
+	}
+
+}
---
+++ PrimerosPasos/src/Variables.java
@@ -0,0 +1,20 @@
+
+public class Variables {
+
+	public static void main(String[] args) {
+		
+		int edad=35; // Inicio variable edad
+		
+		/* La siguientes instrucciones 
+		 * imprime el valor de la variable edad
+		 */
+		
+		System.out.println(edad);
+		
+		edad=75;
+		
+		System.out.println(edad);
+
+	}
+
+}
