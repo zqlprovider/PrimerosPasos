d74220c7b3a669d20e8d5b8b08462de2773d4bf8
Ezequiel <Ezequiel@Ezequiel-PC>
2014-11-13 20:25:39 -0300

    Clase 9 adentro

---
+++ PrimerosPasos/src/Calculos_ConMath.java
@@ -0,0 +1,13 @@
+
+public class Calculos_ConMath {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double raiz=Math.sqrt(9);
+		
+		System.out.println(raiz);
+
+	}
+
+}
