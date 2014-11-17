68453a9ce532a4aa6b6cb95bb5af38f54f0de22e
egallo <egallo@Ezequiel7>
2014-11-17 10:14:18 -0300

    Clase 15 adentro

---
+++ PrimerosPasos/src/EntradaNumeros.java
@@ -0,0 +1,19 @@
+import javax.swing.JOptionPane;
+
+
+public class EntradaNumeros {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			
+		String num1=JOptionPane.showInputDialog("Introduce un número");
+		
+		double num2=Double.parseDouble(num1);
+		
+		System.out.print("La raíz de "+num2+" es ");
+		
+		System.out.printf("%1.2f", Math.sqrt(num2));
+		
+	}
+
+}
