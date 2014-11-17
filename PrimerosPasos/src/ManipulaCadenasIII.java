2582ecc451a5d946bc3b67d477644236253a3941
egallo <egallo@Ezequiel7>
2014-11-17 08:43:57 -0300

    Clase 12 adentro

---
+++ PrimerosPasos/src/ManipulaCadenasIII.java
@@ -0,0 +1,26 @@
+
+public class ManipulaCadenasIII {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String alumno1, alumno2;
+		
+		alumno1="David";
+		alumno2="David";
+		
+		System.out.println(alumno1.equals(alumno2));
+		
+		alumno1="David";
+		alumno2="david";
+		
+		System.out.println(alumno1.equals(alumno2));
+		
+		alumno1="David";
+		alumno2="david";
+		
+		System.out.println(alumno1.equalsIgnoreCase(alumno2));
+
+	}
+
+}
