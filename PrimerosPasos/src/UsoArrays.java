030baddcad97c8ce4514a3ce22fd6bf49baa79c5
Ezequiel <Ezequiel@Ezequiel-PC>
2014-11-18 18:31:08 -0300

    Clase 23 adentro

---
+++ PrimerosPasos/src/UsoArrays.java
@@ -0,0 +1,22 @@
+
+public class UsoArrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*int[] mi_matriz=new int[5];
+		
+		mi_matriz[0]=5;
+		mi_matriz[1]=38;
+		mi_matriz[2]=-15;
+		mi_matriz[3]=92;
+		mi_matriz[4]=71;*/
+		
+		int [] mi_matriz={5, 38, -15, 92, 71, 95, 85, 65, 25, 14, 78};
+		
+		for(int i=0; i<mi_matriz.length; i++){
+			System.out.println("Valor del índice " +i+ "=" + mi_matriz[i]);
+		}
+	}
+
+}
