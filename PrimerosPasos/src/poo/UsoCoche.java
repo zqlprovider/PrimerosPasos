a4986c3a038e8efddc1d588f41538a3d71b3e50f
egallo <egallo@Ezequiel7>
2014-11-25 12:18:30 -0300

    Clase 30-31-32 adentro

--- PrimerosPasos/src/poo/UsoCoche.java
+++ PrimerosPasos/src/poo/UsoCoche.java
@@ -1,10 +1,21 @@
 package poo;
-import javax.swing.*;
 
 public class UsoCoche {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Coche micoche=new Coche();
+		
+		micoche.establece_color("marron");
+		
+		System.out.println(micoche.dime_datos_generales());
+		
+		System.out.println(micoche.dime_color());
+		
+		micoche.configura_asientos("no");
+		
+		System.out.println(micoche.dime_asientos());
 
 	}
 
