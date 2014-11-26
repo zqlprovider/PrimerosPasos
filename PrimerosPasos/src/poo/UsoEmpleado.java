def218cd904b577d8cdba64bff4392718d3e2bb0
Ezequiel <Ezequiel@Ezequiel-PC>
2014-11-26 19:22:17 -0300

    Clase 33 adentro

--- PrimerosPasos/src/poo/UsoEmpleado.java
+++ PrimerosPasos/src/poo/UsoEmpleado.java
@@ -1,5 +1,7 @@
 package poo;
 
+import java.util.*;
+
 public class UsoEmpleado {
 
 	public static void main(String[] args) {
@@ -10,5 +12,38 @@ public class UsoEmpleado {
 }
 
 class Empleado{
+	
+	public Empleado(String nom, double sue, int anio, int mes, int dia){
+		
+		nombre=nom;
+		sueldo=sue;
+		
+		//OJO ENERO ARRANCA EN 0 CERO
+		GregorianCalendar calendario = new GregorianCalendar (anio, mes-1, dia);
+		
+		altaContrato=calendario.getTime();
+		
+	}
+	
+	public String dameNombre(){ //getter
+		return nombre;
+	}
+	
+	public double dameSueldo(){ //getter
+		return sueldo;
+	}
+	
+	public Date dameFechaAlta(){ //getter
+		return altaContrato;
+	}
+	
+	public void subeSueldo(double porcentaje){ //setter
+		double aumento=sueldo*porcentaje/100;
+		sueldo+=aumento;
+	}
+	
+	private String nombre;
+	private double sueldo;
+	private Date altaContrato;
 	
 }
