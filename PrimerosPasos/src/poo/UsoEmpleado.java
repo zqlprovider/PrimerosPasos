1d950f563a5887a6dca0736a4e2028a1fb4c832b
Ezequiel <Ezequiel@Ezequiel-PC>
2014-11-26 20:01:52 -0300

    Clase 35 adentro

--- PrimerosPasos/src/poo/UsoEmpleado.java
+++ PrimerosPasos/src/poo/UsoEmpleado.java
@@ -6,7 +6,49 @@ public class UsoEmpleado {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		/*
+		Empleado empleado1=new Empleado("Ezequiel Gallo", 7000, 2012, 12, 19);
+		
+		Empleado empleado2=new Empleado("Guadalupe Astrada", 5000, 2014, 11, 03);
 
+		Empleado empleado3=new Empleado("Juan Perez", 10000, 2002, 03, 15);
+		
+		empleado1.subeSueldo(5);
+		empleado2.subeSueldo(5);
+		empleado3.subeSueldo(5);
+		
+		System.out.println("Nombre: "+empleado1.dameNombre() + "  Sueldo: " +empleado1.dameSueldo()
+				+ "  Fecha de Alta: " + empleado1.dameFechaAlta());
+		System.out.println("Nombre: "+empleado2.dameNombre() + "  Sueldo: " +empleado2.dameSueldo()
+				+ "  Fecha de Alta: " + empleado2.dameFechaAlta());
+		System.out.println("Nombre: "+empleado3.dameNombre() + "  Sueldo: " +empleado3.dameSueldo()
+				+ "  Fecha de Alta: " + empleado3.dameFechaAlta()); 
+		*/
+		
+		Empleado[] misEmpleados=new Empleado[3];
+		
+		misEmpleados[0]=new Empleado("Ezequiel Gallo", 7000, 2012, 12, 19);
+		misEmpleados[1]=new Empleado("Guadalupe Astrada", 5000, 2014, 11, 03);
+		misEmpleados[2]=new Empleado("Juan Perez", 10000, 2002, 01, 15);
+		
+		/*for(int i=0;i<3;i++){
+			misEmpleados[i].subeSueldo(5);
+		}*/
+		
+		for(Empleado e: misEmpleados){
+			e.subeSueldo(5);
+		}
+		
+		/*for(int i=0;i<3;i++){
+			System.out.println("Nombre: "+misEmpleados[i].dameNombre() + "  Sueldo: " +misEmpleados[i].dameSueldo()
+					+ "  Fecha de Alta: " + misEmpleados[i].dameFechaAlta());
+		}*/
+		
+		for(Empleado e: misEmpleados){
+			System.out.println("Nombre: "+e.dameNombre() + "  Sueldo: " +e.dameSueldo()
+					+ "  Fecha de Alta: " + e.dameFechaAlta());
+		}
 	}
 
 }
